61c42bc1d450bb272d5a88b2dcb8761512a41751
Dan Dee Onions <nadonions@gmail.com>
2024-03-30 07:14:50 +0200

    Update InvoiceHeadPanel.js

--- src/components/InvoiceHeadPanel.js
+++ src/components/InvoiceHeadPanel.js
@@ -8,12 +8,9 @@ import ThirdpartyTypePicker from "../pickers/ThirdpartyTypePicker";
 import { getSubjectAndThirdpartyTypePicker } from "../util/subject-and-thirdparty-picker";
 import InvoiceStatusPicker from "../pickers/InvoiceStatusPicker";
 import { defaultHeadPanelStyles } from "../util/styles";
-import { useDispatch } from 'react-redux';
-
 
 const InvoiceHeadPanel = ({ modulesManager, classes, invoice, mandatoryFieldsEmpty }) => {
   const taxAnalysisTotal = !!invoice?.taxAnalysis ? JSON.parse(invoice.taxAnalysis)?.["total"] : null;
-  const dispatch = useDispatch();
   return (
     <>
       <Grid container className={classes.tableTitle}>
